88b99775ea71c663d6e2f1fca27d48f0fafc7328
Stuart Herbert <stuart@stuartherbert.com>
2015-08-31 22:27:11 +0100

    New: helper for running tests

---
+++ tests/run-tests.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+function die()
+{
+	echo "*** error: $*"
+	exit 1
+}
+
+# the tests are written in PHP
+if ! which php > /dev/null ; then
+	die "the automated tests require PHP" ;
+fi
+
+# we need cURL to download Composer
+if ! which curl > /dev/null ; then
+    die "the automated tests require curl" ;
+fi
+
+# download our dependencies if there is no vendor/ folder
+if [[ ! -e vendor ]] ; then
+
+    # we need PHP's composer to manage packages
+    if [[ ! -e composer.phar ]] ; then
+        curl -sS https://getcomposer.org/installer | php
+    fi
+
+    # install all dependencies
+    if [[ -e composer.lock ]] ; then
+        php ./composer.phar update
+    else
+        php ./composer.phar install
+    fi
+fi
+
+# at this point, Storyplayer and all of its dependencies have been
+# successfully downloaded and installed
+#
+# now we just need to run the tests
+vendor/bin/storyplayer "$@"
