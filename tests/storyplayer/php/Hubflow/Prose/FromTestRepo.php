27fdbe0eb5c427b9cbb124f94b620b689d606d2e
Stuart Herbert <stuart@stuartherbert.com>
2015-09-22 20:41:34 +0100

    New: fromTestRepo()->getGitConfig()

--- tests/storyplayer/php/Hubflow/Prose/FromTestRepo.php
+++ tests/storyplayer/php/Hubflow/Prose/FromTestRepo.php
@@ -2,11 +2,28 @@
 
 namespace Hubflow\Prose;
 
-use GanbaroDigital\Filesystem\Checks\IsFolder;
+use GanbaroDigital\Git\Repo\Checks\IsGitRepo;
+use GanbaroDigital\Git\Repo\ValueBuilders\GetGitConfig;
 use Prose\E5xx_ActionFailed;
 
 class FromTestRepo
 {
+    public function getGitConfig($configName)
+    {
+        // shorthand
+        $path = fromTestRepo()->getPathToLocalRepo();
+
+        // what are we doing?
+        $log = usingLog()->startAction("get Git config '{$configName}' from path '{$path}'");
+
+        // get the config
+        $configValue = GetGitConfig::from(realpath($path), $configName);
+
+        // all done
+        $log->endAction($configValue);
+        return $configValue;
+    }
+
     public function getPathToLocalRepo()
     {
         // what are we doing?
