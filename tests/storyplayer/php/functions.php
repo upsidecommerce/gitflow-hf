ba443cb9b78a2614633dad2088c295b30aa65cec
Stuart Herbert <stuart@stuartherbert.com>
2015-09-22 21:51:31 +0100

    Fix: moved the tests into their own Git repo

--- tests/storyplayer/php/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once(__DIR__ . '/GanbaroDigital/ST2/GitRepo/functions.php');
-require_once(__DIR__ . '/GanbaroDigital/ST2/Hubflow/functions.php');
-require_once(__DIR__ . '/GanbaroDigital/ST2/TestRepo/functions.php');
